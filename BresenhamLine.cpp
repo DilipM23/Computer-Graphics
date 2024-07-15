ec87a8672b298c0216b9bbad7014219941e6e9e3
Dilip M <117372520+DilipM23@users.noreply.github.com>
2024-07-15 16:17:04 +0530

    Update BresenhamLine.cpp

--- BresenhamLine.cpp
+++ BresenhamLine.cpp
@@ -60,7 +60,7 @@ int main(int argc, char ** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("Square");
+	glutCreateWindow("Bresenham Line Drawing");
 	glutDisplayFunc(display);
 	init();
 	glutMainLoop();
